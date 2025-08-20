c04df974764111bff7b82039306fa579837a1f43
Ameericano <ameerakmal2020@gmail.com>
2025-08-20 19:31:36 +0800

    Web Dev Part 2
    Learning New Things, Partially Made A New Blog

---
+++ JavaScript/script.js
@@ -0,0 +1,34 @@
+function rotateGif(element){
+    element.style.transform = "translate(5px, 5px)";
+    element.style.transform = "rotate(180deg)"
+}
+
+function scrolling(){
+    document.getElementById("div").style.color = "blue";
+}
+
+function keypress(){
+    var content = document.getElementById("div")
+    content.innerHTML = "You pressed a key!"
+    content.style.color = "red"
+}
+
+var btn = document.querySelector(".button");
+
+btn.onclick = () => {
+    alert("Hello World!");
+};
+
+btn.onclick = () => {
+    alert("Hello Programmer!");
+};
+
+var btn = document.querySelector(".button2");
+
+btn.addEventListener("click", function(){
+    alert("Hello World!");
+});
+
+btn.addEventListener("click", function(){
+    alert("Hello Programmer!");
+});
