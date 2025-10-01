655b0b0d219292931d8f5846dfce28310797fbc3
Ameericano <ameerakmal2020@gmail.com>
2025-10-01 21:08:18 +0800

    Lesson 15

--- Blog/script.js
+++ Blog/script.js
@@ -6,3 +6,15 @@ function changeText() {
 
     document.getElementById("icon-down").style.display = "block";
 }
+
+var btnSubmit = document.querySelector('.submit');
+var modal = document.querySelector('.modal-container');
+
+if (btnSubmit) {
+    btnSubmit.addEventListener('click', function () {
+        modal.classList.add('show');
+
+        const next = document.querySelector('.next');
+        next.setAttribute('value','https://cobee-the-junior-programmer.netlify.app/contact.html')
+    })
+}
